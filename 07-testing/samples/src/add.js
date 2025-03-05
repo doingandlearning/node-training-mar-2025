60acc8876b08c328addf51ca0ee8c45516037ed2
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-03-05 12:42:17 +0000

    intro to testing

--- 07-testing/samples/src/add.js
+++ 07-testing/samples/src/add.js
@@ -1,3 +1,6 @@
 export function add(a, b) {
+	if (typeof a !== "number" || typeof b !== "number") {
+		throw new TypeError("Both values need to be positive numbers.")
+	}
 	return a + b
 }
--- 07-testing/samples/src/add.test.js
+++ 07-testing/samples/src/add.test.js
@@ -1,6 +1,59 @@
-import { test, expect } from "vitest"
+import { test, expect, describe } from "vitest"
 import { add } from "./add"
 
 test("the happy path", () => {
+	// Arrange - Given
+	// Act - When
+	// Assert - Then
+	const result = add(1, 2)
+	expect(result).toBe(3)
+})
+test("decimals add correctly", () => {
+	// Arrange - Given
+	// Act - When
+	// Assert - Then
+	const result = add(1.2, 2.7)
+	expect(result).toBeCloseTo(3.9, 10)
+})
+test("negatives add correctly", () => {
+	// Arrange - Given
+	// Act - When
+	// Assert - Then
+	const result = add(-1, 2)
+	expect(result).toBe(1)
+})
+
+// Parameterising!
+describe("adding happy path", () => {
+	const testCases = [
+		[2, 3, 5],
+		[-1, 0, -1],
+		[9, 10, 19],
+		[0.1, 0.3, 0.4],
+		[-0.1, -1.2, -1.3]
+	]
+
+	for (const [a, b, expectedResult] of testCases) {
+		test(`add ${a} and ${b} to get ${expectedResult}`, () => {
+			const result = add(a, b)
+			expect(result).toBe(expectedResult)
+		})
+	}
+})
+
+test.each([
+	[2, 3, 5],
+	[-1, 0, -1],
+	[9, 10, 19],
+	[0.1, 0.3, 0.4]
+])("adds %d and %d to get %d", (a, b, expectedResult) => {
+	expect(add(a, b)).toBe(expectedResult)
+})
+
+// 3. Unhappy paths
+test("The add function errors when passed non-numbers", () => {
+	expect(() => add(true, [])).toThrow()
+	expect(() => add(true, [])).toThrowErrorMatchingInlineSnapshot(`[TypeError: Both values need to be positive numbers.]`)
+	expect(() => add(true, [])).toThrowErrorMatchingSnapshot()
 
 })
